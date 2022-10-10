a793155c3d951f92829710003ca881754c2fc528
Jagdeep_singh <44394510+Jaggyy@users.noreply.github.com>
2022-10-10 22:06:35 +0530

    recent one

--- Other/Multer.js
+++ Other/Multer.js
@@ -1,16 +1,1 @@
-const multer = require("multer");
-const crypto = require("crypto");
-const path = require("path");
-const { GridFsStorage } = require("multer-gridfs-storage");
-const Grid = require("gridfs-stream");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/profilepic");
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-});
-// const upload = multer({ storage });
-exports.upload = multer({ storage });
